263ce4670fa730fd1f733227f96fefb15eba3947
jbouv <jbouveret18@gmail.com>
2022-06-01 20:59:38 +0200

    Fxml update

--- Ricochet-Robot/src/main/java/com/example/ricochetrobot/MainController.java
+++ Ricochet-Robot/src/main/java/com/example/ricochetrobot/MainController.java
@@ -1,8 +1,25 @@
 package com.example.ricochetrobot;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
 
 public class MainController {
+    @FXML
+    public GridPane gridPane;
+
+    @FXML
+    public Button jouerBtn;
+
+    @FXML
+    public void onJouerClicked(ActionEvent actionEvent) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource());
+    }
+
 
 }
